8e1b5c9058700f786fdbd8705c6fa0d2c85f045d
Bresia Prudente <macan_rox@yahoo.com>
2019-09-25 16:44:12 -0500

    Updates

---
+++ precache-manifest.d4c7b40fa078615b1164945c3b1cca7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaecdb18b6490861162872e0f0a6dab2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d476dc22b7131f8627a",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "f657e8d91d4317a6541c",
+    "url": "/static/css/main.73381a5c.chunk.css"
+  },
+  {
+    "revision": "2d476dc22b7131f8627a",
+    "url": "/static/js/2.f36140dd.chunk.js"
+  },
+  {
+    "revision": "f657e8d91d4317a6541c",
+    "url": "/static/js/main.8173fa36.chunk.js"
+  },
+  {
+    "revision": "ee19c9178cda611897fb",
+    "url": "/static/js/runtime~main.9376060f.js"
+  }
+]);
